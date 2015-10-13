d96e434b136ce945af2b0be209d134a0c2c133c5
sopanhavuth-aka-sam <sopanhavuth.som@gmail.com>
2015-10-13 12:43:36 -0700

    correct files

--- src/Display.java
+++ src/Display.java
@@ -8,10 +8,9 @@ public class Display extends JFrame {
 	private InfoPane info = new InfoPane();
 	
 	public Display(){  
-<<<<<<< HEAD
 		setSize(1000,750);  
 		setLayout(new FlowLayout());  //change later
-=======
+
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int) screenSize.getHeight();
 		int width = (int) screenSize.getWidth();
@@ -20,33 +19,30 @@ public class Display extends JFrame {
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
->>>>>>> 97558ac1aa6d637f4c4da0d6d4e3590a62940504
+
 		
 		//adding ImagePane
 		
 		//testing drawPlayer////////
-		drawPlayer("John", 72, 148, 1);
-		drawPlayer("Mary", 432, 40, 1);
-		drawPlayer("John2", 72, 148, 2);
+		drawToken("John", 72, 148, 1);
+		drawToken("Mary", 432, 40, 1);
+		drawToken("John2", 72, 148, 2);
 		////////////////////////////
 		
 		JScrollPane scrollImage = new JScrollPane(testImage);
-<<<<<<< HEAD
 		scrollImage.setPreferredSize(new Dimension(1000,500));
 		this.getContentPane().add(scrollImage);
 		
 		//adding controlPane
-		this.add(testPane);
+		this.add(info);
 		
 		this.setVisible(true);
-=======
 		scrollImage.setPreferredSize(new Dimension((int)(width/1.05),(int)(height/1.5)));
 		this.getContentPane().add(scrollImage, BorderLayout.NORTH);
 		//adding controlPane
 		//this.add(testPane);
 		this.add(info, BorderLayout.SOUTH);
->>>>>>> 97558ac1aa6d637f4c4da0d6d4e3590a62940504
-		}
+	}
 
 	public void drawToken(String name, int x, int y, int drawPos) {
 		
--- src/main.java
+++ src/main.java
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-import java.util.Arrays;
 
-=======
-<<<<<<< HEAD
+import java.util.Arrays;
 import javax.swing.JFrame;
-=======
->>>>>>> cbee60bde4d8974d54ff5ec19cb3cbbd6d29c93f
->>>>>>> 97558ac1aa6d637f4c4da0d6d4e3590a62940504
+
 
 public class main {
 	
 public static void main(String[] args) {  
-	
-	Board testBoard = new Board();
-	System.out.println(testBoard.getName(0));
-	System.out.println(Arrays.toString(testBoard.getConnectedRoom(0)));
-	System.out.println(testBoard.getLoc(0));
-	System.out.println(testBoard.getPos(0));
-	
-	Display testDisplay = new Display();
-	
-<<<<<<< HEAD
-	
-	}  
-=======
+
+	Controller gameControl = new Controller();
+
 }  
->>>>>>> cbee60bde4d8974d54ff5ec19cb3cbbd6d29c93f
+
 }  
